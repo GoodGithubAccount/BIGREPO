c6c20856e1f8e109e30c843746ae7602f4803383
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-03-06 15:23:32 +0100

    Add JUnit dependency

---
+++ backend/src/test/kotlin/project/github/backend/BackendUnitTests.kt
@@ -0,0 +1,12 @@
+package project.github.backend
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class BackendUnitTests {
+
+    @Test
+    fun `example test`() {
+        assertEquals(4, 2+2)
+    }
+}
