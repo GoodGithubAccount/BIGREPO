b10f87dee1075f9989d542429b5625b00134f9f1
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-03-07 01:34:10 +0100

    Add H2 database and JPA
    H2 will be our in-memory database and JPA is the
     Java persistence API; this will let us map
     objects to relational database tables.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.0"
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
+	kotlin("plugin.jpa") version "1.7.22"
 }
 
 group = "project.github"
@@ -16,10 +17,13 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
 	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
