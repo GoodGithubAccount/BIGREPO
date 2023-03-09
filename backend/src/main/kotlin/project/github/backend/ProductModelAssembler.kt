06f66abc4fdab1d30f36accb85c74c3ffe7c45b8
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-03-09 22:01:48 +0100

    Add a model assembler for products
    This will convert Product objects into its
    EntityModel representation with self-links and a
    link to all products.
    
    The class is marked as a
    @Component which automatically creates it on
    application startup.

---
+++ backend/src/main/kotlin/project/github/backend/ProductModelAssembler.kt
@@ -0,0 +1,31 @@
+package project.github.backend
+
+import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.server.RepresentationModelAssembler
+import org.springframework.hateoas.server.core.DummyInvocationUtils.methodOn
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
+import org.springframework.stereotype.Component
+
+/**
+ * A [RepresentationModelAssembler] implementation that converts the domain type [Product] object
+ * into its [EntityModel] representation.
+ *
+ * The class is marked as [Component] and will automatically be created on startup.
+ */
+@Component
+class ProductModelAssembler : RepresentationModelAssembler<Product, EntityModel<Product>> {
+
+    /**
+     * Converts the non-model object [product] to the equivalent model-based object
+     * [EntityModel] of the [product] including the self link and link to all products.
+     *
+     * @param product the non-model object to be converted.
+     * @return the converted model-based object.
+     */
+    override fun toModel(product: Product): EntityModel<Product> {
+        return EntityModel.of(product,
+                linkTo(methodOn(ProductController::class.java).getProduct(product.getId())).withSelfRel(),
+                linkTo(methodOn(ProductController::class.java).all()).withRel("products")
+        )
+    }
+}
