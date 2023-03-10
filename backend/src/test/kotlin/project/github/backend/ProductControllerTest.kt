55f1a80e0fa5ecb55ac21a608951cb81ca441f1d
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-03-10 21:13:40 +0100

    Reconfigure test suite and add basic tests

--- backend/src/test/kotlin/project/github/backend/ProductControllerTest.kt
+++ backend/src/test/kotlin/project/github/backend/ProductControllerTest.kt
@@ -1,28 +1,52 @@
 package project.github.backend
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.boot.test.mock.mockito.MockBeans
-import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.*
+import org.springframework.http.HttpStatus
+import org.springframework.test.annotation.DirtiesContext
 
-@WebMvcTest(ProductController::class)
-@AutoConfigureMockMvc
-@MockBeans(
-        MockBean(ProductModelAssembler::class),
-        MockBean(ProductRepository::class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = [
+            "spring.datasource.url=jdbc:h2:mem:testdb"
+        ]
 )
-class ProductControllerTest {
-    @Autowired
-    private lateinit var mockMvc: MockMvc
+class ProductControllerTest(@Autowired val client: TestRestTemplate) {
 
     @Test
-    fun `should return ok message`() {
-        mockMvc.perform(get("/products"))
-                .andExpect(status().isOk)
+    fun `retrieving all products returns 200`() {
+        val entity = client.getForEntity<String>("/products")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+    }
+
+    @Test
+    fun `retrieving a non-existent product returns 404`() {
+        val id = "unique-id"
+        val entity = client.getForEntity<String>("/products/$id")
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.NOT_FOUND)
+    }
+
+    @DirtiesContext
+    @Test
+    fun `saving a product returns the product and saves it`() {
+        val productId = "unique-id"
+        val product = Product(
+                id = productId,
+                name = "",
+                price = 0,
+                currency = "",
+                rebateQuantity = 0,
+                rebatePercent = 0,
+                upsellProduct = "null"
+        )
+
+        val postResponse = client.postForObject<Product>("/products", product)
+
+        assertThat(postResponse).isEqualTo(product)
+        val getResponse = client.getForObject<Product>("/products/$productId")
+        assertThat(getResponse).isEqualTo(product)
     }
 }
