0355ce8c57aa80ff828189d3e26a014ab9198a79
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-03-20 00:25:35 +0100

    Add preloaded Order example info

--- backend/src/main/kotlin/project/github/backend/LoadDatabase.kt
+++ backend/src/main/kotlin/project/github/backend/LoadDatabase.kt
@@ -5,6 +5,9 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import project.github.backend.order.Order
+import project.github.backend.order.OrderRepository
+import project.github.backend.order.Status
 import project.github.backend.product.Product
 import project.github.backend.product.ProductRepository
 
@@ -19,11 +22,20 @@ class LoadDatabase {
     /**
      * Returns a [CommandLineRunner] instance which is used to run the initialization code
      * after the application context has been loaded.
-     * @param repository A [ProductRepository] instance which is used to save the pre-defined data to the database.
+     * @param productRepository A [ProductRepository] instance which is used to save the pre-defined data.
+     * @param orderRepository An [OrderRepository] instance which is used to save the pre-defined data.
      */
     @Bean
-    fun initDatabase(repository: ProductRepository) = CommandLineRunner {
-        log.info("Preloading " + repository.save(Product("vitamin-d-90-100", "D-vitamin, 90ug, 100 stk", 116, "DKK", 3, 10, null)))
-        log.info("Preloading " + repository.save(Product("vitamin-c-500-250", "C-vitamin, 500mg, 250 stk", 150, "DKK", 2, 25, "vitamin-c-depot-500-250")))
+    fun initDatabase(productRepository: ProductRepository, orderRepository: OrderRepository) = CommandLineRunner {
+        println("Preloading...")
+        val p1 = Product("vitamin-d-90-100", "D-vitamin, 90ug, 100 stk", 116, "DKK", 3, 10, null)
+        val p2 = Product("vitamin-c-500-250", "C-vitamin, 500mg, 250 stk", 150, "DKK", 2, 25, "vitamin-c-depot-500-250")
+        val o1 = Order( Status.IN_PROGRESS, listOf(p1,p2))
+        val o2 = Order(Status.IN_PROGRESS, listOf(p2))
+
+        log.info("Preloading " + productRepository.save(p1))
+        log.info("Preloading " + productRepository.save(p2))
+        log.info("Preloading " + orderRepository.save(o1))
+        log.info("Preloading " + orderRepository.save(o2))
     }
 }
