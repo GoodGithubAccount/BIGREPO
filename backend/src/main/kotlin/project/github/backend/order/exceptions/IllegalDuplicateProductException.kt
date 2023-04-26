33913791d3b31c05c13400becb5a3496ab30664d
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-04-27 00:44:10 +0200

    Add KDoc to IllegalDuplicateProductException

--- backend/src/main/kotlin/project/github/backend/order/exceptions/IllegalDuplicateProductException.kt
+++ backend/src/main/kotlin/project/github/backend/order/exceptions/IllegalDuplicateProductException.kt
@@ -1,4 +1,7 @@
 package project.github.backend.order.exceptions
 
+/**
+ * An exception thrown when an order is created two of the same product.
+ */
 class IllegalDuplicateProductException(productId: String) :
     IllegalOrderOperationException("Failed to create order with duplicate of product $productId")
