303eecff0b208a196c67f67a69070c04e99f1728
mads05dk <90009716+mads05dk@users.noreply.github.com>
2023-04-28 14:50:17 +0200

    More_Comments
    Added comments to more parts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,15 @@ import './App.css';
 import './index.css';
 import Route from './Components/Route';
 
-
 function App() {
 	return (
 		<div>
 			<h2>SHOPshop</h2>
 			<Route />
-			<h4>SHOPshop</h4>
+			<h4 id='footer'>SHOPshop - The best place to shop!</h4>
 		</div>
 	);
 }
 
 export default App;
+
--- frontend/src/Pages.tsx
+++ frontend/src/Pages.tsx
@@ -3,10 +3,13 @@ import ShoppingHandler from './ShoppingHandler';
 import TestPage from './Pages/TestPage';
 // Import other page components here
 
+// `Pages` is an object that contains functions for rendering each step of the checkout flow.
+// Each function returns a React component that represents the content for that step.
 const Pages = {
-	1: () => <ShoppingHandler />,
-	2: () => <TestPage />,
-	// Add other pages here
+	1: () => <ShoppingHandler />, // Step 1 shows the shopping cart and item details
+	2: () => <TestPage />, // Step 2 is a test page for demonstration purposes
+	// Add other page components here
 };
 
+// Export the `Pages` object for use in other components.
 export default Pages;
