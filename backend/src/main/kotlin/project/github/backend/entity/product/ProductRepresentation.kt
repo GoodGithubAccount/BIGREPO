912b36cb27378a6f143274f3b6e0fcea9aa4603f
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-04-29 23:47:29 +0200

    Change ProductRepresentation properties
    Allows easier testing

--- backend/src/main/kotlin/project/github/backend/entity/product/ProductRepresentation.kt
+++ backend/src/main/kotlin/project/github/backend/entity/product/ProductRepresentation.kt
@@ -8,11 +8,11 @@ import java.math.BigDecimal
  * It is the form in which the client uses to create a product.
  */
 class ProductRepresentation : RepresentationModel<ProductRepresentation>() {
-    val id: String? = null
-    val name: String? =  null
-    val price: BigDecimal? = null
-    val currency: String? = null //TODO currency verification https://docs.spring.io/spring-hateoas/docs/current/reference/html/#mediatypes.hal-forms.options
-    val rebateQuantity: Int? = null
-    val rebatePercent: BigDecimal? = null
-    val upsellProductId: String? = null //TODO runtime verification?
+    var id: String? = null
+    var name: String? =  null
+    var price: BigDecimal? = null
+    var currency: String? = null //TODO currency verification https://docs.spring.io/spring-hateoas/docs/current/reference/html/#mediatypes.hal-forms.options
+    var rebateQuantity: Int? = null
+    var rebatePercent: BigDecimal? = null
+    var upsellProductId: String? = null //TODO runtime verification?
 }
