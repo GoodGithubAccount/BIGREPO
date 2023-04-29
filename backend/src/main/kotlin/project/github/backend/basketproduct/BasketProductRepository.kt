2969c7591c44679dafc5ddada7171334f776e2f2
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-04-29 19:57:26 +0200

    Add BasketProductRepository

---
+++ backend/src/main/kotlin/project/github/backend/basketproduct/BasketProductRepository.kt
@@ -0,0 +1,5 @@
+package project.github.backend.basketproduct
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface BasketProductRepository : JpaRepository<BasketProduct, Long>
