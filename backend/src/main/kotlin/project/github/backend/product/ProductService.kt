faac8248108cecde2eff2e63f8f9c289157ce58d
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-04-29 21:05:37 +0200

    Add ProductService

---
+++ backend/src/main/kotlin/project/github/backend/product/ProductService.kt
@@ -0,0 +1,21 @@
+package project.github.backend.product
+
+import org.springframework.stereotype.Service
+
+/**
+ * Service that provides [Product] related operations with the database.
+ */
+@Service
+class ProductService(
+    private val productRepository: ProductRepository
+) {
+    /**
+     * Fetches a [Product] by its id from the database.
+     * @param id the id of the product.
+     * @return the product with the given id.
+     * @throws ProductNotFoundException if no product with the given id is found.
+     */
+    fun getProduct(id: String): Product {
+        return productRepository.findById(id).orElseThrow { ProductNotFoundException(id) }
+    }
+}
