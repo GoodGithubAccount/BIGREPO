1c372a92e6b097f1a7fbbea0fc3d6cbc0b888701
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-05-02 01:42:27 +0200

    Add CORS config
    I hate frontend

---
+++ backend/src/main/kotlin/project/github/backend/security/WebConfig.kt
@@ -0,0 +1,17 @@
+package project.github.backend.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+
+    override fun addCorsMappings(corsRegistry: CorsRegistry) {
+        corsRegistry.addMapping("/**")
+            .allowedOrigins("https://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("Content-Type", "Authorization")
+            .allowCredentials(true)
+    }
+}
