5faf8d710568689b79b6d7ff20c86c3e461f4863
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-05-01 03:32:11 +0200

    Add getBasket and getBasketProduct

--- backend/src/main/kotlin/project/github/backend/entity/basket/BasketController.kt
+++ backend/src/main/kotlin/project/github/backend/entity/basket/BasketController.kt
@@ -1,23 +1,32 @@
 package project.github.backend.entity.basket
 
 import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.server.ExposesResourceFor
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import project.github.backend.entity.basketproduct.BasketProduct
+import project.github.backend.entity.basket.basketproduct.BasketProduct
+import project.github.backend.entity.basket.basketproduct.BasketProductModelAssembler
 
 @RestController
+@ExposesResourceFor(Basket::class)
 @RequestMapping("/basket")
-class BasketController {
+class BasketController(
+    private val basketService: BasketService,
+    private val basketAssembler: BasketModelAssembler,
+    private val basketProductAssembler: BasketProductModelAssembler
+) {
 
     @GetMapping("/{id}")
     fun getBasket(@PathVariable id: Long): EntityModel<Basket> {
-        TODO("Not yet implemented")
+        val basket = basketService.getBasket(id)
+        return basketAssembler.toModel(basket)
     }
 
     @GetMapping("/{basketId}/products/{productId}")
-    fun getBasketProduct(@PathVariable basketId: String, @PathVariable productId: String): EntityModel<BasketProduct> {
-        TODO("Not yet implemented")
+    fun getBasketProduct(@PathVariable basketId: Long, @PathVariable productId: Long): EntityModel<BasketProduct> {
+        val basketProduct = basketService.getBasketProduct(basketId, productId)
+        return basketProductAssembler.toModel(basketProduct)
     }
 }
