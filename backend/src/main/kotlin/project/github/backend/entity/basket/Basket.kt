9eb14c4cb3ab01382221de9f32b3e1df91490b9e
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-05-01 03:25:13 +0200

    Add related order as property

--- backend/src/main/kotlin/project/github/backend/entity/basket/Basket.kt
+++ backend/src/main/kotlin/project/github/backend/entity/basket/Basket.kt
@@ -1,16 +1,18 @@
 package project.github.backend.entity.basket
 
 import com.fasterxml.jackson.annotation.JsonIgnore
-import jakarta.persistence.CascadeType
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
-import jakarta.persistence.OneToMany
-import project.github.backend.entity.basketproduct.BasketProduct
+import jakarta.persistence.*
+import project.github.backend.entity.basket.basketproduct.BasketProduct
+import project.github.backend.entity.order.Order
 
 @Entity
 data class Basket(
     @Id @GeneratedValue @JsonIgnore val id: Long? = null,
-    @OneToMany(cascade = [CascadeType.ALL]) val products: List<BasketProduct>, //TODO add HREF link
-    val numberOfProducts: Int,
-)
+    @OneToMany(mappedBy = "basket", cascade = [CascadeType.ALL]) var products: List<BasketProduct>, //TODO add HREF link
+    var numberOfProducts: Int,
+    @OneToOne(mappedBy = "basket", fetch = FetchType.LAZY) @JsonIgnore var order: Order? = null
+) {
+    override fun toString(): String {
+        return "Basket(id=$id, products=$products, numberOfProducts=$numberOfProducts, orderId=${order?.id})"
+    }
+}
