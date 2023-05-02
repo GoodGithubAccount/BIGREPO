1cb1b1431ab118caa79029b1e880d8a2394eaefc
Hamsterpants <kevinhoejgaard@gmail.com>
2023-05-02 02:33:24 +0200

    Add https support for dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteBasicSslPlugin()],
   test: {
     environment: "jsdom",
     setupFiles: ["vitest.setup.ts"],
