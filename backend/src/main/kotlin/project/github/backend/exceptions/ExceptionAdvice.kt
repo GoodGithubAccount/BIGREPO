00b22469cd3982cb292cc41d453ebedb54b3476c
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-05-02 02:13:05 +0200

    Change return error to be ProblemDetails
    instead of custom ErrorDetails

--- backend/src/main/kotlin/project/github/backend/exceptions/ExceptionAdvice.kt
+++ backend/src/main/kotlin/project/github/backend/exceptions/ExceptionAdvice.kt
@@ -3,15 +3,14 @@ package project.github.backend.exceptions
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.context.request.ServletWebRequest
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 import org.springframework.web.server.ResponseStatusException
-import java.time.Instant
 import java.util.regex.Pattern
 
 /**
@@ -33,47 +32,42 @@ class ExceptionAdvice {
     /**
      * Handles all exceptions of type [MethodArgumentTypeMismatchException].
      * @param e The [MethodArgumentTypeMismatchException] instance which is thrown.
-     * @return [ErrorDetails] object with the details of the exception.
+     * @return [ResponseEntity] of type [ProblemDetail] object with the details of the exception.
      */
     @ResponseBody
     @ExceptionHandler(MethodArgumentTypeMismatchException::class)
     fun methodArgumentTypeMismatchHandler(
         e: MethodArgumentTypeMismatchException,
         request: WebRequest
-    ): ResponseEntity<ErrorDetails> {
-        return handleException(BackendException(e.message, HttpStatus.BAD_REQUEST), request)
+    ): ResponseEntity<ProblemDetail> {
+        return handleException(BackendException(e.message, HttpStatus.BAD_REQUEST))
     }
 
     /**
      * Handles all custom [BackendException] instances.
      * @param e The [BackendException] instance which is thrown.
-     * @return [ErrorDetails] object with the details of the exception.
+     * @return [ResponseEntity] of type [ProblemDetail] object with the details of the exception.
      */
     @ResponseBody
     @ExceptionHandler(BackendException::class)
-    fun backendExceptionHandler(e: BackendException, request: WebRequest): ResponseEntity<ErrorDetails> {
-        return handleException(e, request)
+    fun backendExceptionHandler(e: BackendException): ResponseEntity<ProblemDetail> {
+        return handleException(e)
     }
 
     /**
-     * Formats the exception and returns an [ErrorDetails] object.
+     * Formats the exception and returns an [ProblemDetail] object with an HTTPS status code.
      * @param e The [BackendException] instance which is thrown.
-     * @param request The [WebRequest] metadata at the time of the exception.
-     * @return [ErrorDetails] object with the details of the exception as well as the HTTP status.
+     * @return [ResponseEntity] of type [ProblemDetail] object with
+     * the details of the exception as well as the HTTP status.
      */
-    private fun handleException(e: BackendException, request: WebRequest): ResponseEntity<ErrorDetails> {
+    private fun handleException(e: BackendException): ResponseEntity<ProblemDetail> {
         log.warn(e.message)
         val status = e.status
         val message = cleanMessage(e.message)
 
-        val errorDetails = ErrorDetails(
-            timestamp = Instant.now().toString(),
-            status = status.value(),
-            error = status.reasonPhrase,
-            message = message,
-            path = (request as ServletWebRequest).request.requestURI
-        )
-        return ResponseEntity(errorDetails, status)
+        val problemDetail = ProblemDetail.forStatusAndDetail(status, message)
+
+        return ResponseEntity.status(status.value()).body(problemDetail)//(errorDetails, status)
     }
 
     /**
