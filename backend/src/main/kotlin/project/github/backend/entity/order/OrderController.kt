bbcd4fd169d2c118363f19987d8c27e1759c344c
SebastianHalfdanLauridsen <s215769@dtu.dk>
2023-05-19 16:53:38 +0200

    Rework GET all orders endpoint

--- backend/src/main/kotlin/project/github/backend/entity/order/OrderController.kt
+++ backend/src/main/kotlin/project/github/backend/entity/order/OrderController.kt
@@ -2,18 +2,17 @@ package project.github.backend.entity.order
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.hateoas.CollectionModel
 import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.mediatype.hal.HalModelBuilder
 import org.springframework.hateoas.server.ExposesResourceFor
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*
+import org.springframework.http.HttpEntity
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import project.github.backend.LoadDatabase
 import project.github.backend.entity.order.exceptions.IllegalOrderCancellationException
 import project.github.backend.entity.order.exceptions.IllegalOrderCompletionException
 import project.github.backend.entity.order.exceptions.OrderNotFoundException
-import java.util.stream.Collectors
 
 @RestController
 @ExposesResourceFor(Order::class)
@@ -29,15 +28,28 @@ class OrderController(
      * TODO KDoc
      */
     @GetMapping
-    fun orders(): CollectionModel<*> {
-        val ordersStream = orderService.getAllOrders().stream()
-        val ordersAsEntityModel = ordersStream.map { order ->
-            assembler.toModel(order)
-        }.collect(Collectors.toList())
+    fun orders(): HttpEntity<*> {
+        val allOrders = orderService.getAllOrders()
 
-        return CollectionModel.of(
-            ordersAsEntityModel, linkTo(methodOn(OrderController::class.java).orders()).withSelfRel()
-        )
+        val proxyControllerClass = methodOn(OrderController::class.java)
+
+        val selfLink = linkTo(proxyControllerClass.orders()).withSelfRel()
+                .andAffordance(afford(proxyControllerClass.createOrder(null)))
+
+        val findLink = linkTo(proxyControllerClass.getOrder(null)).withRel("find")
+
+        val orderLinks = allOrders.map { order ->
+            linkTo(proxyControllerClass.getOrder(order.id)).withRel("product")
+                    .withTitle(order.id.toString())
+        }
+
+        val model = HalModelBuilder.emptyHalModel()
+                .link(selfLink)
+                .link(findLink)
+                .links(orderLinks)
+                .build()
+
+        return ResponseEntity.ok(model)
     }
 
     /**
@@ -60,7 +72,10 @@ class OrderController(
      * @throws OrderNotFoundException If no order with the specified [id] is found in the [OrderRepository].
      */
     @GetMapping("/{id}")
-    fun getOrder(@PathVariable id: Long): EntityModel<Order> {
+    fun getOrder(@PathVariable id: Long?): EntityModel<Order> {
+        if (id == null) {
+            throw OrderNotFoundException(null)
+        }
         val order = orderService.getOrder(id)
         return assembler.toModel(order)
     }
